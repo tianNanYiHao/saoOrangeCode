344e2aff2e0a898ed9ae3ebeebc2c84b23181e31
liuff <liu.ff_zf@sand.com.cn>
2017-06-07 13:52:03 +0800

    Initial Commit

---
+++ saoOrangeCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  saoOrangeCode
+//
+//  Created by tianNanYiHao on 2017/6/7.
+//  Copyright © 2017年 tianNanYiHao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
